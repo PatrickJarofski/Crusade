b2a3d9d6eeff0ef5a1c41154ca7fd05c3d38d6de
PatrickJarofski <patrick.jarofski@oit.edu>
2014-10-17 14:44:27 -0700

    Initial Server

---
+++ CrusadeSeniorProject/CrusadeServer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrusadeServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stubbed Server.");
+            Console.Read();
+        }
+    }
+}
