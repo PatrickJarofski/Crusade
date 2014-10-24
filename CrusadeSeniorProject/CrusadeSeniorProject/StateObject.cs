491d2d747e5ebd8cfdf46774ccc173da9921d1e3
PatrickJarofski <patrick.jarofski@oit.edu>
2014-10-23 17:11:13 -0700

    stateObject

---
+++ CrusadeSeniorProject/CrusadeSeniorProject/StateObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrusadeSeniorProject
+{
+    public class StateObject
+    {
+        public const int BufferSize = 1024;
+
+        public Socket workSocket = null;
+        public byte[] buffer = new byte[BufferSize];
+        public StringBuilder stringBuilder = new StringBuilder();
+    }
+}
