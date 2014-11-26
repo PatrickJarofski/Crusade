62b17a722f65fed0df2962838be3d21ba0ee9568
PatrickJarofski <patrick.jarofski@oit.edu>
2014-11-26 11:33:24 -0800

    add state object

---
+++ CrusadeSeniorProject/CrusadeServer/StateObject.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrusadeServer
+{
+    public class StateObject
+    {
+        public const int BufferSize = 1024;
+        public byte[] buffer = new byte[BufferSize];
+        public System.Net.Sockets.Socket workerSocket;
+        public StringBuilder sb = new StringBuilder();
+
+        public void Clear()
+        {
+            buffer = new byte[BufferSize];
+            sb.Clear();
+        }
+
+    }
+}
