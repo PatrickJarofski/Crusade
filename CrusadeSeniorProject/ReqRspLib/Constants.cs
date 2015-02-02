1a02cb6b180de0c3712c9c12672be4d1f12bcc18
PatrickJarofski <patrick.jarofski@oit.edu>
2015-02-02 15:47:27 -0800

    constants

---
+++ CrusadeSeniorProject/ReqRspLib/Constants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReqRspLib
+{
+    public static class Constants
+    {
+        public static const byte TYPE_TROOP = 0x01;
+        public static const byte TYPE_EQUIP = 0x02;
+        public static const byte TYPE_FIELD = 0x03;
+
+        // NoLocation, Deck, Hand, Field, Grave
+
+        public static const byte LOCATION_NONE = 0x10;
+        public static const byte LOCATION_DECK = 0x11;
+        public static const byte LOCATION_HAND = 0x12;
+        public static const byte LOCATION_FIELD = 0x13;
+        public static const byte LOCATION_GRAVE = 0x14;
+    }
+}
